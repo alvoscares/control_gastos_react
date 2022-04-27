56a6f0e88c8cce729b86a67eeeffa507d0f0be41
Alvoscares <alvoscares@gmail.com>
2022-04-27 15:00:11 -0300

    Mensaje de error en Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
+import Modal from './components/Modal'
 
 function App() {
 
-  const [presupuesto, setPresupuesto] = useState(0)
+  const [presupuesto, setPresupuesto] = useState('')
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
   const [modal, setModal] = useState(false)
+  const [animarModal, setAnimarModal] = useState(false)
 
-  const handleNuevoGasto = () =>{
-    setModal(true)    
+  const handleNuevoGasto = () => {
+    setModal(true)
+
+    setTimeout(() => {
+      setAnimarModal(true)
+    }, 500);
   }
 
   return (
@@ -30,7 +36,12 @@ function App() {
         </div>
       )}
 
-      {modal && <p>Desde Modal</p>}
+      {modal &&
+        <Modal
+          setModal={setModal}
+          animarModal={animarModal}
+          setAnimarModal={setAnimarModal}
+        />}
 
     </div>
   )
